7c608b2107c1a94be68076c93cbcb3cf314a4666
Gera <oko.hi@yandex.ru>
2026-04-18 11:00:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ludum58
+
+Создано на Unreal Engine 5.7.4
---
+++ Source/Ludum58/Ludum58.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Ludum58.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Ludum58, "Ludum58" );
